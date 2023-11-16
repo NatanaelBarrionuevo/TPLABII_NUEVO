1551a335fd8c53d9ccd334dcb76f62de589de5af
GabrielScipioni <gabriel.scipioni21@gmail.com>
2023-11-16 00:06:35 -0300

    version rulos

---
+++ TindaMusica/TindaMusica/principal.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace menuslide
+{
+    public partial class principal : Form
+    {
+        bool sidebarexp =true;
+        private Form currentChildForm;
+        public principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sidebartimer_Tick(object sender, EventArgs e)
+        {
+            if (sidebarexp)
+            {
+                
+
+                //maneja la velocidad de animacion
+                sidebar.Width -= 50;
+
+
+                //si esta expandido, que se minimize 
+                if (sidebar.Width==sidebar.MinimumSize.Width)
+                {
+                    sidebarexp = false;
+                    sidebartimer.Stop();
+                }
+            }
+            else
+            {       //maneja la velocidad de animacion
+                sidebar.Width += 50;
+                //si esta minimizado que se expanda
+                if (sidebar.Width==sidebar.MaximumSize.Width)
+                {
+                    sidebarexp = true;
+                    sidebartimer.Stop();
+                }
+            }
+        }
+
+        #region menubtn
+
+        
+        private void button5_Click(object sender, EventArgs e)
+        {
+            sidebartimer.Start();
+            if (pnlayuda.Size == pnlayuda.MaximumSize)
+            {
+                pnlayuda.Height = pnlayuda.MinimumSize.Height;
+            }
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion
+        #region barra min y cerrar
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            this.WindowState=FormWindowState.Minimized;
+        }
+        #endregion
+        
+        private void OpenChildForm(Form childForm)
+        {
+            //open only form
+            if (currentChildForm != null)
+            {
+                currentChildForm.Close();
+            }
+            currentChildForm = childForm;
+            //End
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            pnlhijos.Controls.Add(childForm);
+            pnlhijos.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+        }
+
+        private void btninformacion_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new informacion());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnresumen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnayuda_Click(object sender, EventArgs e)
+        {
+            if (sidebarexp == true)
+            {
+                submenu(pnlayuda);
+            }
+            if (sidebarexp)
+            {
+                //maneja la velocidad de animacion
+                sidebar.Width += 50;
+                //si esta minimizado que se expanda
+                if (sidebar.Width == sidebar.MaximumSize.Width)
+                {
+                    sidebarexp = true;
+                    sidebartimer.Stop();
+                }
+            }
+
+        }
+
+        private void submenu(Panel panel)
+        {
+            //si esta max que se min
+            if (panel.Size == panel.MaximumSize)
+            {
+                panel.Height = panel.MinimumSize.Height;
+            }
+            //si esta max que se min
+            else
+                panel.Height = panel.MaximumSize.Height;
+        }
+
+        private void pnlbar_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        int posX;
+        int posY;
+        private void pnlbar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button !=MouseButtons.Left)
+            {
+                posX=e.X;
+                posY=e.Y;
+            }
+            else
+            {
+                Left=Left +(e.X - posX);
+                Top=Top+(e.Y - posY);
+            }
+        }
+    }
+}
